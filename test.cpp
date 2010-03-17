f3dbde5610623e9ed7d6e2b9d5a36e193b044bf9
luoq <luoq08@gmail.com>
2010-03-17 22:38:26 +0800

    加入operator()

--- test.cpp
+++ test.cpp
@@ -1,41 +1,72 @@
-// test.cpp --- 
-// 
+// test.cpp ---
+//
 // Filename: test.cpp
-// Description: test for iSparseMatrix 
+// Description: test for iSparseMatrix
 // Author: Luo Qiang
 // Created: 03/17/2010 16:42:44
-// Version: 
-// Last-Updated: 03/17/2010 20:34:01
+// Version:
+// Last-Updated: 03/17/2010 22:34:55
 //           By: Luo Qiang
-//     Update #: 12
-// Keywords: 
+//     Update #: 23
+// Keywords:
 
-// Commentary: 
+// Commentary:
 
 // Change log:
 
 // Code:
 #include "iSparseMatrix.h"
+
+char getch() {
+  char a;
+  cin >> a;
+  cin.ignore(1024, '\n');
+  return a;
+}
 void test_set(smat<int> &m)
 {
   int r,c,e;
-  while(1)
-    {
-      cout<<"Input row,column,element"<<endl;
-      cin>>r>>c>>e;
-      m.set(r,c,e);
-      cout<<m<<endl;
-    }
+  cout<<"set element\n";
+  cout<<"Input row,column,element"<<endl;
+  cin>>r>>c>>e;
+  m.set(r,c,e);
+  cout<<m<<endl;
+}
+void test_get(const smat<int> &m)
+{
+  cout<<"get element\n";
+  cout<<"Input row,column :\n";
+  int r,c;
+  cin>>r>>c;
+  cout<<m(r,c)<<endl;
 }
 int main()
 {
-  smat<int>	m(10,10,3);
-  cout<<m<<endl;
-  test_set(m);
-  return 0;
+  smat<int>	m(100,100,3);
+  while(1)
+    {
+      cout<<"set(s),get(g),print(p)?\n";
+      char	choice = getch();
+      switch(choice)
+	{
+	case 's':
+	  test_set(m);
+	  break;
+	case 'g':
+	  test_get(m);
+	  break;
+	case 'p':
+	  cout<<m;
+	  break;
+	case 'q':
+	  return 0;
+	default:
+	  break;
+	}
+    }
 }
 
 
 
-// 
+//
 // test.cpp ends here
